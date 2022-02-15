aa8ea8ebfccb6f49a522e7fc8b6dc4b3deb8e5cd
Git-KJ <98420203+Git-KJ@users.noreply.github.com>
2022-02-15 16:38:30 -0700

    Add files via upload

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -104,7 +104,6 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
       var chosenCategoryShortName = chooseRandomCategory (categories)
-      console.log("Short name is " + chosenCategoryShortName.short_name)
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -120,7 +119,6 @@ function buildAndShowHomeHTML (categories) {
       // var homeHtmlToInsertIntoMainPage = ....
       
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'")
-      console.log(homeHtmlToInsertIntoMainPage)
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
