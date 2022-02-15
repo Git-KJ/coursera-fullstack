35139bc19066b16c4266f9f26d5dbfe842432852
Git-KJ <98420203+Git-KJ@users.noreply.github.com>
2022-02-15 15:03:33 -0700

    Add files via upload

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
       
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName",chosenCategoryShortName.short_name)
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", chosenCategoryShortName.short_name)
       console.log(homeHtmlToInsertIntoMainPage)
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
